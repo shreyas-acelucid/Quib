f29efc9630668c2b6a0e6f622131013b50c2461a
Acedev@ck <chandan.kumar@acelucid.com>
2023-04-19 11:07:55 +0530

    change environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   BASE_URL:"http://13.126.212.31:5000",
   JSON_SERVER:" http://localhost:3000",
-  QUIB_ADMIN:"http://3.88.43.237"
+  QUIB_ADMIN:"http://44.211.90.48"
 };
