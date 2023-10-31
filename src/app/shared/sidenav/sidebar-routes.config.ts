4769281b462b48eecc1b09fce5cae7c037a64368
Hemanth Katte <hemanthss24@gmail.com>
2023-10-31 16:53:28 +0530

    made the quib list header more compact

--- src/app/shared/sidenav/sidebar-routes.config.ts
+++ src/app/shared/sidenav/sidebar-routes.config.ts
@@ -1,32 +1,33 @@
 export const SideNaveMenueRoute = [
-    {
-        label: 'User',
-        icon: 'pi pi-fw pi-user',
-        routerLink: ['/customer/quib-user'],
-    },
-    {
-        label: 'Movies',
-        icon: 'pi pi-inbox',
-        routerLink: ['/Movies/all-movies'],
-        // items: [
-        //     {
-        //         label: 'All Movies',
-        //         routerLink: ['/Movies/all-movies']
-        //     },
-        //     {
-        //         label: 'Active Movies',
-        //         routerLink: ['/Movies/active-movies']
-        //     },
-        // ]
-    },
-    {
-        label: 'Quibs',
-        // routerLink: ['/Quib/recent-quib'],
-        items: [
-            {
-                label: 'Quib List',
-                routerLink: ['/Quib/recent-quib']
-            },
-        ]
-    },
-]
+  {
+    label: 'User',
+    icon: 'pi pi-fw pi-user',
+    routerLink: ['/customer/quib-user'],
+  },
+  {
+    label: 'Movies',
+    icon: 'pi pi-inbox',
+    routerLink: ['/Movies/all-movies'],
+    // items: [
+    //     {
+    //         label: 'All Movies',
+    //         routerLink: ['/Movies/all-movies']
+    //     },
+    //     {
+    //         label: 'Active Movies',
+    //         routerLink: ['/Movies/active-movies']
+    //     },
+    // ]
+  },
+  {
+    label: 'Quibs',
+    icon: 'pi pi-database',
+    routerLink: ['/Quib/recent-quib'],
+    // items: [
+    //     {
+    //         label: 'Quib List',
+    //         routerLink: ['/Quib/recent-quib']
+    //     },
+    // ]
+  },
+];
