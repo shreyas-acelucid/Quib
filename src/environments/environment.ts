692e069dd36b9a32dbe09f93155f26e27ce2d278
Hemanth Katte <hemanthss24@gmail.com>
2024-07-11 19:23:53 +0530

    environment variable changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   BASE_URL: 'http://13.126.212.31:5000',
-  QUIB_ADMIN: 'http://44.211.90.48'
+  //QUIB_ADMIN: 'http://44.211.90.48',
+  QUIB_ADMIN: 'https://quibs.com'
 };
